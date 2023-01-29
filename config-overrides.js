80cf93831cb9b6ff3a398474197d7cfc39a8ef55
Grony <gr0ny.0017@gmail.com>
2023-01-29 22:04:09 +0300

    -

--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,6 @@ const { override, addWebpackAlias } = require("customize-cra");
 
 const path = require("path");
 
-
 module.exports = override(
   addWebpackAlias({
     "/src": path.resolve(__dirname, "src")
